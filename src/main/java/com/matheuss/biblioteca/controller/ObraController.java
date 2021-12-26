f39ba25d3310616289819943595a66a897acde27
Matheus Silva <85570707+matheusdsilva01@users.noreply.github.com>
2021-12-26 19:12:48 -0300

    Update ObraController.java

--- src/main/java/com/matheuss/biblioteca/controller/ObraController.java
+++ src/main/java/com/matheuss/biblioteca/controller/ObraController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/obra")
 public class ObraController {
