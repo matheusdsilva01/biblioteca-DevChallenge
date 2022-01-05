0d5579082888c8420b8f6bac8edfb7ff617b95f1
Matheus da silva <ms25022003@gmail.com>
2022-01-05 16:48:49 -0300

    update CrossOrigin

--- src/main/java/com/matheuss/biblioteca/controller/ObraController.java
+++ src/main/java/com/matheuss/biblioteca/controller/ObraController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://front-end-biblioteca.vercel.app/"})
 @RestController
 @RequestMapping("/obra")
 public class ObraController {
